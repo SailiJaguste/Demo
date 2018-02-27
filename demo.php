71d21f25b33a27d0bbade80bd6f0b0bb5dfe3821
CORP\saili.jaguste <saili.jaguste@intelliswift.co.in>
2018-02-27 17:27:38 +0530

    Commit1

--- demo.php
+++ demo.php
@@ -1,11 +1,3 @@
 <?php
 echo "This is a demo PHP Page";
-echo "IS THIS WORKING???";
-echo "YES IT IS...!!!";
-echo "Lets test again";
-echo "Final Test";
-echo "AGAIN";
-echo "POSITIVE>>>>>>";
-echo "FAILED...";
-echo "HOPE IT WORKS"
 ?>
